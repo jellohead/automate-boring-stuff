4b2d2420b7991be740b44826f59a6ec41a69c71b
Pat Escalona <pat.escalona7@gmail.com>
2022-10-28 20:16:49 -0500

    Finished ch16

--- sect-06/ch14.py
+++ sect-06/ch14.py
@@ -1,2 +1,26 @@
 # Ch 14 for loops with lists, multiple assignment, and
 # augmented operators
+
+supplies = ['pens', 'staplers', 'tools', 'desk', 'computer']
+for i in range(len(supplies)):
+    print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
+
+# multiple assignment
+cat = ['fat', 'orange', 'loud']
+size, color, disposition = cat
+print(size)
+print(color)
+print(disposition)
+
+# variable swapping
+a = 'AAA'
+b = 'BBB'
+a, b = b, a
+print(a)
+print(b)
+
+
+# augmented assignment operators
+spam = 42
+spam = spam + 1
+spam += 1  # this works with most math operators + - * / %
